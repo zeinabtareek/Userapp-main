23d72efbde04302724d0376b320a6b475eca5084
secretdev <zinabtarek1998@gmail.com>
2023-09-19 16:51:30 +0300

    notification

--- lib/view/screens/parcel/check_rates_screen.dart
+++ lib/view/screens/parcel/check_rates_screen.dart
@@ -6,8 +6,12 @@ import 'package:ride_sharing_user_app/view/screens/parcel/widgets/route_widget.d
 
 import '../../../util/app_strings.dart';
 import '../../../util/app_style.dart';
+import '../../../util/dimensions.dart';
+import '../../../util/text_style.dart';
 import '../../widgets/custom_app_bar.dart';
 import '../../widgets/custom_body.dart';
+import '../../widgets/custom_calender.dart';
+import '../../widgets/custom_drop_down.dart';
 
 class CheckRatesScreen extends StatelessWidget {
   const CheckRatesScreen({super.key});
@@ -46,9 +50,65 @@ class CheckRatesScreen extends StatelessWidget {
                           colorText: Colors.white),
                     ),
                     // Expanded(child: SizedBox()),
+                    Container(
+                      width: 120,
+                      height: 35,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context)
+                            .primaryColor
+                            .withOpacity(0.06),
+                        borderRadius: BorderRadius.circular(
+                            Dimensions.radiusOverLarge),
+                        border: Border.all(
+                          width: 1,
+                          color: Theme.of(context)
+                              .primaryColor
+                              .withOpacity(0.2),
+                        ),
+                      ),
+                      // padding:  K.fixedPadding0,
+                      child: Center(
+                        child: CustomDropDown(
+                          icon: Icon(
+                            Icons.expand_more,
+                            color: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .color!
+                                .withOpacity(0.5),
+                          ),
+                          maxListHeight: 200,
+                          items: x
+                              .map((item) =>
+                              CustomDropdownMenuItem<String>(
+                                value: item,
+                                child: Text(
+                                  item.tr,
+                                  style: textRegular.copyWith(
+                                      color:
+                                      item != x.first
+                                          ? Theme.of(context)
+                                          .textTheme
+                                          .bodyMedium!
+                                          .color!
+                                          .withOpacity(0.5)
+                                          : Theme.of(context)
+                                          .primaryColor),
+                                ),
+                              ))
+                              .toList(),
+                          hintText: Strings.select.tr,
+                          borderRadius: 5,
+                          onChanged: (selectedItem) {
+
+                          },
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
             )));
   }
 }
+final List<String> x = ['1 kg', '2 kg', '3 kg', '4 kg'];
